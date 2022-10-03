94e1e5dd8a312cc799478018bc3be88f1c708948
Matheus <wallysom2@gmail.com>
2022-10-03 15:49:18 -0300

    feat: configurando bibliotecas

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express, { json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(json());
+app.use(router);
+
+app.listen(4000, () => {
+  console.log("Server listening on port 4000");
+});
